d46439603d7310064f17d7b781918e22c98ae27d
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-05-15 20:26:11 +0530

    UI updates

--- frontend/src/components/BlogCard.js
+++ frontend/src/components/BlogCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import formatDate from '../utils/formatDate';
+import ReactMarkdown from 'react-markdown';
 
 const BlogCard = ({ blog }) => {
   const { slug, title, image, description, category, date } = blog;
@@ -13,7 +14,9 @@ const BlogCard = ({ blog }) => {
       <Link to={`/blog/${slug}`} className="blog">
         <article className="info">
           <h4 className="blog-title">{title}</h4>
-          <p className="blog-description">{finalDescription}</p>
+          <p className="blog-description">
+            <ReactMarkdown>{finalDescription}</ReactMarkdown>
+          </p>
           <div className="blog-footer">
             <small className="blog-category">{category}</small>
             <p className="blog-date">
